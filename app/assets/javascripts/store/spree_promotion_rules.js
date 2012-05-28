6f1cde93c975ef773bca19991268403b4d047cf7
Patrick McElwee <pmcelwee@gmail.com>
2012-05-27 23:40:56 -0300

    Move Coffeescript from store to admin

--- app/assets/javascripts/store/spree_promotion_rules.js
+++ app/assets/javascripts/store/spree_promotion_rules.js
@@ -1,9 +1,1 @@
 //= require store/spree_core
-jQuery ($) ->
-  $('.tokeninput.roles').tokenInput("/admin/roles.json?" + "authenticity_token=" + escape(AUTH_TOKEN), {
-    searchDelay          : 600,
-    hintText             : strings.type_to_search,
-    noResultsText        : strings.no_results,
-    searchingText        : strings.searching,
-    processPrePopulate   : true
-  })
