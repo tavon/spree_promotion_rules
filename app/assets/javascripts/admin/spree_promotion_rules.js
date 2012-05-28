6b2657e5c0b961304781f91ab5c18c747e204051
Patrick McElwee <pmcelwee@gmail.com>
2012-05-27 23:42:19 -0300

    Adding to admin js

--- app/assets/javascripts/admin/spree_promotion_rules.js
+++ app/assets/javascripts/admin/spree_promotion_rules.js
@@ -1,1 +1,9 @@
 //= require admin/spree_core
+jQuery ($) ->
+  $('.tokeninput.roles').tokenInput("/admin/roles.json?" + "authenticity_token=" + escape(AUTH_TOKEN), {
+    searchDelay : 600,
+    hintText : strings.type_to_search,
+    noResultsText : strings.no_results,
+    searchingText : strings.searching,
+    processPrePopulate : true
+  })
